5538c3463bea373bf00318e5a895a15880157afb
Serhii Mumriak <sergiy.mumryak@gmail.com>
2023-09-20 11:23:25 -0700

    Beep boop

--- extras/controlled_filament_sensor.py
+++ extras/controlled_filament_sensor.py
@@ -1,14 +1,14 @@
 from . import filament_switch_sensor
 
-class ControlledFilamentSensor(filament_switch_sensor.RunoutHelper):
+class ControlledFilamentSensor(filament_switch_sensor.SwitchSensor):
     def __init__(self, config):
         super(ControlledFilamentSensor, self).__init__(config)
 
     def get_runout_pause(self):
-        return self.runout_pause
+        return self.runout_helper.runout_pause
 
     def set_runout_pause(self, newValue):
-        self.runout_pause = newValue
+        self.runout_helper.runout_pause = newValue
 
 def load_config_prefix(config):
     return ControlledFilamentSensor(config)
